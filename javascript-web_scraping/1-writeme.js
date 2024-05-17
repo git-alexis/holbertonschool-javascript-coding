2ba85bfe401d7299d9f4624860a563e054e0562b
git-alexis <alex59.billemont@gmail.com>
2024-05-17 08:56:47 +0200

    add comment

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,9 +2,11 @@
 
 const fs = require('fs');
 
+// Extracting command line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
 
+// Writing content to file
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
