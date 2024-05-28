8ac224cb69532dce4d70ed2d1039681c88f19d8e
git-alexis <alex59.billemont@gmail.com>
2024-05-28 14:45:35 +0200

    program named 1-stdin.js that will be executed through command line and display: name in input in three sentences

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,8 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  console.log('This important software is now closing');
-  process.exit(0);
+process.stdin.on('data', (input) => {
+  const name = input.toString();
+  process.stdout.write(`Your name is: ${name}`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
