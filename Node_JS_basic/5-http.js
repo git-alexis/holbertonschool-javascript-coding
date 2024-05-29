43246fc0d07af41a1cf75ad5393cdd4f2c2b80c7
git-alexis <alex59.billemont@gmail.com>
2024-05-29 22:19:23 +0200

    create a more complex HTTP server using the http module

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+const database = process.argv[2];
+
+const app = http.createServer(async (request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+
+  if (request.url === '/') {
+    response.write('Hello Holberton School!');
+    response.end();
+  }
+
+  if (request.url === '/students') {
+    response.write('This is the list of our students\n');
+    countStudents(database)
+      .then((result) => {
+        response.write(`${result.totalNumber}\n`);
+        response.write(`${result.CS}\n`);
+        response.write(`${result.SWE}`);
+        response.end();
+      })
+      .catch((error) => {
+        console.error(error);
+        response.end();
+      });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
