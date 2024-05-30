e101fe160b34c07538949840b5b7a85585dea2c7
git-alexis <alex59.billemont@gmail.com>
2024-05-30 14:18:10 +0200

    It read the database asynchronously, return a promise and reject the promise with the error or return an object of arrays of the firstname of students per fields

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -2,24 +2,24 @@ import fs from 'fs';
 
 function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
-    try {
-      const data = fs.readFile(filePath, 'utf-8');
-      const lines = data.split('\n').filter(Boolean);
-      const students = lines.slice(1).map((line) => line.split(','));
-      const fields = {};
-
-      for (const student of students) {
-        const [firstname, , , field] = student;
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
+    fs.readFile(filePath, 'utf-8', (error, data) => {
+      if (error) {
+        console.error('Error:', error);
+        reject(new Error('Cannot load the database'));
+        return;
       }
 
-      resolve(fields);
-    } catch (error) {
-      reject(new Error('Cannot load the database'));
-    }
+      const groupedByField = data
+        .split('\n')
+        .filter(Boolean)
+        .slice(1)
+        .reduce((accumulator, line) => {
+          const [firstname, , , field] = line.split(',');
+          accumulator[field] = [...(accumulator[field] || []), firstname];
+          return accumulator;
+        }, {});
+      resolve(groupedByField);
+    });
   });
 }
 
