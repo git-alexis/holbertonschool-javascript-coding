2b59ec59bf4144ae013b7b2075e3cf3301daac59
git-alexis <alex59.billemont@gmail.com>
2024-05-31 13:45:50 +0200

    fix eslint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,33 +3,23 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-
-    const lines = data.split('\n').filter(Boolean);
-
-    const students = lines.slice(1);
-
+    const rows = data.split('\n').filter(Boolean);
+    const students = rows.slice(1).map((row) => {
+      const [firstName, lastName, age, field] = row.split(',');
+      return {
+        firstName,
+        lastName,
+        age,
+        field,
+      };
+    });
+    const csStudents = students.filter((student) => student.field === 'CS');
+    const sweStudents = students.filter((student) => student.field === 'SWE');
     console.log(`Number of students: ${students.length}`);
-
-    const fieldCounts = students.reduce((counts, line) => {
-      const field = line.split(',')[3];
-
-      if (!counts[field]) {
-        counts[field] = { count: 0, names: [] };
-      }
-
-      counts[field].count += 1;
-      counts[field].names.push(line.split(',')[0]);
-
-      return counts;
-    }, {});
-
-    for (const field in fieldCounts) {
-      console.log(`Number of students in ${field}: ${fieldCounts[field].count}. List: ${fieldCounts[field].names.join(', ')}`);
-    }
-
-  } catch (err) {
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.map((student) => student.firstName).join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map((student) => student.firstName).join(', ')}`);
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
-
 module.exports = countStudents;
